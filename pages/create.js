4466830d1ed3e5ce30d3dfb86aa6c6ec1483ffa7
kritika243 <24kritikasharma24@gmail.com>
2021-10-25 16:46:18 +0530

    other pages from navbar

---
+++ pages/create.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+const Create = () => {
+  return (
+    <div>
+      <h1>Create Page</h1>
+    </div>
+  )
+}
+
+export default Create
---
+++ pages/signup.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+const Signup = () => {
+  return (
+    <div>
+      <h1>SignUp Page</h1>
+    </div>
+  )
+}
+
+export default Signup
