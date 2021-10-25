b48f845a60028250bcdcf18537a131b4db30267c
kritika243 <24kritikasharma24@gmail.com>
2021-10-25 15:36:03 +0530

    removed non essential code

--- pages/index.js
+++ pages/index.js
@@ -6,25 +6,8 @@ const Home = (prop) => {
       <h1>Home Page</h1>
       <h2>{prop.message}</h2>
       <Link href='/products'>View All Products</Link>
-      <style jsx>
-        {`
-          h1 {
-            color: red;
-          }
-        `}
-      </style>
     </div>
   )
-}
-
-// works on the server side, is used when page is not user specific
-export async function getStaticProps(context) {
-  const res = await fetch('http://localhost:3000/api/test')
-  const data = await res.json()
-  console.log(data) // logged on the server console
-  return {
-    props: { message: data.message }, // will be passed to the page component as props
-  }
 }
 
 export default Home
