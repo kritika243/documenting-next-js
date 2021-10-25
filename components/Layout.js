d97f5fc712b425fff46daa608f7232cc89d959b8
kritika243 <24kritikasharma24@gmail.com>
2021-10-25 16:34:59 +0530

    UI - navbar

--- components/Layout.js
+++ components/Layout.js
@@ -9,6 +9,10 @@ const Layout = ({ children }) => {
           rel='stylesheet'
           href='https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css'
         />
+        <link
+          href='https://fonts.googleapis.com/icon?family=Material+Icons'
+          rel='stylesheet'
+        ></link>
       </Head>
       <Navbar />
       {children}
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,36 @@
+import Link from 'next/link'
+
 const Navbar = () => {
-  return <h1>Navbar</h1>
+  return (
+    <>
+      <nav>
+        <div className='nav-wrapper #7e57c2 deep-purple lighten-1'>
+          <Link href='/'>
+            <a class='brand-logo left'>Shoppers</a>
+          </Link>
+          <ul id='nav-mobile' className='right'>
+            <li>
+              <Link href='/login'>
+                <a>Login</a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/signup'>
+                <a>SignUp</a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/create'>
+                <a>
+                  <i className='large material-icons'>add_to_photos</i>
+                </a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  )
 }
 
 export default Navbar
