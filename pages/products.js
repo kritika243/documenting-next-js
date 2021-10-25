5db76cc4062e0d8060527cdbfefa6f4c56a98abf
kritika243 <24kritikasharma24@gmail.com>
2021-10-25 13:51:06 +0530

    fetching data

---
+++ pages/products.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const Products = () => {
+  return (
+    <div>
+      <h1>All products page</h1>
+      <Link href='/'>Back to Home</Link>
+    </div>
+  )
+}
+
+export default Products
