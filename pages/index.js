bc2caf4cb1e0c40c6ed7c8a35c65b536e29b40a5
kritika243 <24kritikasharma24@gmail.com>
2021-10-26 16:18:25 +0530

    rendering the products after fetching from database

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,35 @@
 import Link from 'next/link'
 
-const Home = (prop) => {
-  return (
-    <div>
-      <h1>Home Page</h1>
-      <h2>{prop.message}</h2>
-      <Link href='/products'>View All Products</Link>
-    </div>
-  )
+const Home = ({ products }) => {
+  const productList = products.map((product) => {
+    return (
+      <div className='card'>
+        <div className='card-image'>
+          <img src={product.mediaUrl} />
+          <span className='card-title'>{product.name}</span>
+        </div>
+        <div className='card-content'>
+          <p>Rs {product.price}</p>
+        </div>
+        <div className='card-action'>
+          <a href='#'>This is a link</a>
+        </div>
+      </div>
+    )
+  })
+
+  // console.log(products)
+  return <div className='rootCard'>{productList}</div>
+}
+
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:3000/api/products')
+  const data = await res.json()
+  return {
+    props: {
+      products: data,
+    },
+  }
 }
 
 export default Home
