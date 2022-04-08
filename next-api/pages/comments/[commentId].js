48ad95e56e9073a9be3f0511cd52db9e81284d4e
kritika243 <24kritikasharma24@gmail.com>
2022-04-08 15:35:03 +0530

    pre rendering and api routes

---
+++ next-api/pages/comments/[commentId].js
@@ -0,0 +1,36 @@
+import { comments } from '../../data/comments'
+
+function Comment({ comment }) {
+  return (
+    <>
+      <div>{comment.text}</div>
+      <hr />
+    </>
+  )
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { commentId: '1' } },
+      { params: { commentId: '2' } },
+      { params: { commentId: '3' } },
+    ],
+    fallback: false,
+  }
+}
+
+export async function getStaticProps(context) {
+  const { params } = context
+  const { commentId } = params
+
+  const comment = comments.find((comment) => comment.id === parseInt(commentId))
+
+  return {
+    props: {
+      comment,
+    },
+  }
+}
+
+export default Comment
