e67849341e965d448b12f922b2c455f765cef255
kritika243 <24kritikasharma24@gmail.com>
2022-04-08 11:53:15 +0530

    making a GET request from frontend and making its api route

---
+++ next-api/pages/comments/index.js
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+function CommentsPage() {
+  const [comments, setComments] = useState([])
+  async function fetchComments(async) {
+    const response = await fetch('/api/comments')
+    const data = await response.json()
+    setComments(data)
+  }
+  return (
+    <>
+      <button onClick={fetchComments}>Load Comments</button>
+      {comments.map((comment) => {
+        return <div key={comment.id}>{comment.text}</div>
+      })}
+    </>
+  )
+}
+
+export default CommentsPage
