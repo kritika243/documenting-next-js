6b1c69a16b4768bdaf61b767df98a6c9796f43c1
kritika243 <24kritikasharma24@gmail.com>
2022-04-17 19:23:30 +0530

    placeholder set to blur for Image

--- extras/pages/pets.js
+++ extras/pages/pets.js
@@ -1,9 +1,17 @@
 import Image from 'next/image'
+import Image0 from '../public/0.jpg'
 
 function PetsPage() {
   return (
     <>
       <div>
+        <Image
+          src={Image0}
+          alt='image of pets'
+          width={'280'}
+          height='280'
+          placeholder='blur'
+        />
         {['1', '2', '3', '4'].map((path) => {
           return (
             <div key={path}>
