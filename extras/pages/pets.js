980e30ee24d62dfd6693807d13650c404a66c631
kritika243 <24kritikasharma24@gmail.com>
2022-04-18 15:24:08 +0530

    implementing static html export

--- extras/pages/pets.js
+++ extras/pages/pets.js
@@ -1,28 +1,11 @@
-import Image from 'next/image'
 import Image0 from '../public/0.jpg'
 
 function PetsPage() {
   return (
     <>
       <div>
-        <Image
-          src={Image0}
-          alt='image of pets'
-          width={'280'}
-          height='280'
-          placeholder='blur'
-        />
         {['1', '2', '3', '4'].map((path) => {
-          return (
-            <div key={path}>
-              <Image
-                src={`/${path}.jpeg`}
-                alt='pet image'
-                width='280'
-                height='280'
-              />
-            </div>
-          )
+          return <div key={path}>pets</div>
         })}
       </div>
     </>
